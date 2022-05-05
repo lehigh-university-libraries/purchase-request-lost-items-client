aae405bd5f3d2aa4041de1f6f908ac56e621639b
Maccabee Levine <msl321@lehigh.edu>
2022-05-05 15:36:02 -0400

    Create base spring mvn project

---
+++ src/main/java/edu/lehigh/libraries/purchase_request/lost_items_client/LostItemsApplication.java
@@ -0,0 +1,24 @@
+package edu.lehigh.libraries.purchase_request.lost_items_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication
+@Slf4j
+public class LostItemsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LostItemsApplication.class);
+	}
+
+	public static void main(String[] args) {
+		log.info("Starting the Lost Items Application");
+		SpringApplication.run(LostItemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/lehigh/libraries/purchase_request/lost_items_client/LostItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lehigh.libraries.purchase_request.lost_items_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
