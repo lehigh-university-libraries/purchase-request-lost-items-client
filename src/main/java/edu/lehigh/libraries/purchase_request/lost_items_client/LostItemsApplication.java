bdd7065648a7af31f6bb7aab7510af0d1bcaa03c
Maccabee Levine <msl321@lehigh.edu>
2023-10-20 09:04:11 -0400

    Log build time

--- src/main/java/edu/lehigh/libraries/purchase_request/lost_items_client/LostItemsApplication.java
+++ src/main/java/edu/lehigh/libraries/purchase_request/lost_items_client/LostItemsApplication.java
@@ -3,8 +3,11 @@ package edu.lehigh.libraries.purchase_request.lost_items_client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.context.WebApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,9 +21,24 @@ public class LostItemsApplication extends SpringBootServletInitializer {
 		return application.sources(LostItemsApplication.class);
 	}
 
+	@Override
+	protected WebApplicationContext run(SpringApplication application) {
+		WebApplicationContext context = super.run(application);
+		reportBuild(context);
+		return context;
+	}
+
+	private static void reportBuild(ApplicationContext context) {
+		BuildProperties buildProperties = context.getBean(BuildProperties.class);
+		log.info("Build time: " + buildProperties.getTime());
+	}
+
 	public static void main(String[] args) {
 		log.info("Starting the Lost Items Application");
+		ApplicationContext context = SpringApplication.run(LostItemsApplication.class, args);
+		reportBuild(context);
 		SpringApplication.run(LostItemsApplication.class, args);
+        log.info("Lost Items Application started");
 	}
 
 }
